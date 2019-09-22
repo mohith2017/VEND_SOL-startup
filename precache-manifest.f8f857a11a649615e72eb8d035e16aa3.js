edb77cb8b00dd148e262ed805f7f1e14277af235
unknown <mohith.m.venkat@gmail.com>
2019-09-22 13:43:10 +0530

    Updates

---
+++ precache-manifest.f8f857a11a649615e72eb8d035e16aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784904faddf4f6a031ec35ff9ddbcc2a",
+    "url": "/website_demo/index.html"
+  },
+  {
+    "revision": "4ae9623acca5c1ec6cce",
+    "url": "/website_demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d7743d458e5e0bda454b",
+    "url": "/website_demo/static/js/2.e4e9f23e.chunk.js"
+  },
+  {
+    "revision": "4ae9623acca5c1ec6cce",
+    "url": "/website_demo/static/js/main.85acd69b.chunk.js"
+  },
+  {
+    "revision": "9c942eaf87229c76924b",
+    "url": "/website_demo/static/js/runtime-main.faca6f93.js"
+  },
+  {
+    "revision": "443a33a41d28a9eed5128f6424e86594",
+    "url": "/website_demo/static/media/quinoa-seeds.443a33a4.png"
+  }
+]);
